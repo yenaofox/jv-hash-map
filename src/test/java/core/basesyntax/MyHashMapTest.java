2212b06be595667d32bda29ecc867e03b6b3c64e
boroda4436 <chupika.bohdan@ukr.net>
2019-08-29 16:54:35 +0300

    added empty test cases

--- src/test/java/core/basesyntax/MyHashMapTest.java
+++ src/test/java/core/basesyntax/MyHashMapTest.java
@@ -1,5 +1,57 @@
 package core.basesyntax;
 
+import org.junit.Test;
+
 public class MyHashMapTest {
 
+    @Test
+    public void getValueOk() {
+
+    }
+
+    @Test
+    public void getValueByNullKey() {
+
+    }
+
+    @Test
+    public void getValueWithCollision() {
+        //TODO: use PLane
+    }
+
+    @Test
+    public void getByNonExistedKey() {
+
+    }
+
+    @Test
+    public void getTheOverriddenValueByKey() {
+
+    }
+
+    @Test
+    public void putOk() {
+
+    }
+
+    @Test
+    public void putByNullKey() {
+
+    }
+
+    @Test
+    public void putWithCollision() {
+        //TODO: use PLane
+    }
+
+
+    @Test
+    public void putTheOverriddenValueByKey() {
+
+    }
+
+    @Test
+    public void checkTheHashMapIncrease() {
+        //TODO: add 1000 elements
+    }
 }
