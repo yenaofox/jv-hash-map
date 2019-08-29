3d9b7f224d301e92ef0e64e3b48203325898506c
Sofasmile <sofiya.dyakonova@ukr.net>
2019-08-29 18:04:57 +0300

    added test for hash map

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,24 @@
 package core.basesyntax;
 
 /**
- * <p>Реалізувати свою HashMap, а саме методи `put(K key, V value)` та `getValue()`. Дотриматися
- * основних вимог щодо реалізації мапи (initial capacity, load factor, resize...) За бажанням
- * можна реалізувати інші методи інтрефейсу Map.</p>
+ * <p>Реалізувати свою HashMap, а саме методи `put(K key, V value)`, `getValue()` та `getSize()`.
+ * Дотриматися основних вимог щодо реалізації мапи (initial capacity, load factor, resize...)
+ * За бажанням можна реалізувати інші методи інтрефейсу Map.</p>
  */
 public class MyHashMap<K, V> implements MyMap<K, V> {
 
     @Override
-    public V put(K key, V value) {
-        return null;
+    public void put(K key, V value) {
+
     }
 
     @Override
     public V getValue(K key) {
         return null;
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
     }
 }
